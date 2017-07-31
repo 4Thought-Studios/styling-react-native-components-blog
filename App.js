9968ca79bccaf422fc194002ac0b70b0b67a214d
Brad Dunn <brad@braddunn.com>
2017-07-31 16:30:05 -0400

    Button style

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 const Dot = (props) => {
   let currentStyle = props.active ? styles.dotActive : styles.dotInactive;
   return (
     <View style={[styles.dot, currentStyle]}></View>
+  );
+};
+
+const NextButton = (props) => {
+  return (
+    <TouchableOpacity 
+      style={styles.nextButton}
+      onPress={props.onPress}
+    >
+      <Text style={styles.nextButtonText}>Next</Text>
+    </TouchableOpacity>
   );
 };
 
@@ -18,8 +29,10 @@ export default class App extends React.Component {
           <Dot/>
         </View>
         <Image style={styles.calendarImage} source={require('./assets/Calendar.png')} />
-        <Text>Keep your work organized and quickly check your reminders with simple calendar.</Text>
-        <Text>Next</Text>
+        <Text style={styles.text}>Keep your work organized and quickly check your reminders with simple calendar.</Text>
+        <NextButton 
+          onPress={() => console.log('Next Button Pressed!')} 
+        />
       </View>
     );
   }
@@ -53,5 +66,24 @@ const styles = StyleSheet.create({
   calendarImage: {
     width: 180,
     height: 180
+  },
+  text: {
+    textAlign: 'center',
+    paddingLeft: 60,
+    paddingRight: 60,
+    fontSize: 18,
+    lineHeight: 30,
+  },
+  nextButton: {
+    width: '100%',
+    backgroundColor: '#FC3768',
+    paddingTop: 20,
+    paddingBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nextButtonText: {
+    fontSize: 20,
+    color: 'white',
   }
 });
